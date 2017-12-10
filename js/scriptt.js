a84d6e8655360f5d8db8fcde3ce682cc57dd0ba4
H-dnsys <33608255+H-dnsys@users.noreply.github.com>
2017-12-10 17:02:10 -0500

    Add files via upload

---
+++ js/scriptt.js
@@ -0,0 +1,27 @@
+/*var $nav= $('dir');
+var $navTop = $nav.offset().top;
+var pegarNav = function(){
+	var $scrollTop = $(window).scrollTop();
+	if ($scrollTop>=$divTop) {
+		$nav.addClass('stiky')
+	}else{
+		$nav.removeClass('stiky')
+	}
+}
+$(window).on('scroll',pegarNav);
+
+$(window).on('scroll',function () {
+	var $scrollTop = $(window).scrollTop();
+	$('.stiky').text('el escrol es '+ $scrollTop +'px');
+});*/
+
+
+
+
+/*$(window).on('scroll',function () {
+	$('body').css('background','red');
+});*/
+
+
+
+
